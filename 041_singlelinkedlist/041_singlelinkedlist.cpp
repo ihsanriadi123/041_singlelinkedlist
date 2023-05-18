780d0f97438e8dcd02133c9b285fe56a3066c07c
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-18 16:09:09 +0700

    menampilkan kode dari mahasiswa

--- 041_singlelinkedlist/041_singlelinkedlist.cpp
+++ 041_singlelinkedlist/041_singlelinkedlist.cpp
@@ -7,3 +7,25 @@ struct node {
 	node* next;
 };
 
+node* start = NULL;
+
+void addnode() {
+	int nim;
+	string nama;
+	node* nodebaru = new node();
+	cout << "masukkan nim: ";
+	cin >> nim;
+	cout << "masukkan nama: ";
+	cin >> nama;
+	nodebaru -> noMhs = nim;
+	nodebaru->name = nama;
+
+	if (start == NULL || nim <= start->noMhs) {
+		if (start != NULL && nim == start->noMhs)
+		{
+			cout << "nim sudah ada" << endl;
+			return 0;
+		}
+
+
+	}
